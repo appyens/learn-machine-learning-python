9ac1b85227180c454c05fd8db29d71624486e7d6
appyens <an.anurag@msn.com>
2019-05-31 02:15:44 +0530

    simple linear regression notes added

--- part2_regression/simple_linear_regression.py
+++ part2_regression/simple_linear_regression.py
@@ -5,21 +5,40 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
 resource = os.path.join(__root__.PROJECT_ROOT, 'data/simple_linear_regression/')
 csv = os.path.join(resource, 'Salary_Data.csv')
 # reading dataset with read.csv
 dataset = pd.read_csv(csv)
+
+# independent variable, predictor, attribute
 x = dataset.iloc[:, :-1].values
-# dependent variable
+# dependent variable, response, label
 y = dataset.iloc[:, 1].values
+
 # train test split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
 # fitting simple linear regression to the training set
 regressor = LinearRegression()
 regressor.fit(x_train, y_train)
+
+# to retrieve the intercept
+print(regressor.intercept_)
+# to retrieve the slope
+# This means that for every one unit of change in X , the change in the Y is about 9312%
+print(regressor.coef_)
+
 # predicting the test set results
+# Now that we have trained our algorithm, it’s time to make some predictions.
+# To do so, we will use our test data and see how accurately our algorithm predicts the percentage score.
 y_pred = regressor.predict(x_test)
+
+# Now compare the actual output values for X_test with the predicted values, execute the following script:
+compare = pd.DataFrame({"Actual": y_test, 'Predicted': y_pred})
+
+# predicting train set results
 x_pred = regressor.predict(x_train)
 
 # visualising the training set results
@@ -39,3 +58,15 @@ plt.title("Salary Vs Experience (Test set)")
 plt.xlabel("Years of experience")
 plt.ylabel("Salary")
 plt.show()
+
+# The final step is to evaluate the performance of the algorithm. This step is particularly important
+# to compare how well different algorithms perform on a particular dataset.
+# For regression algorithms, three evaluation metrics are commonly used:
+
+# Mean Absolute Error (MAE) is the mean of the absolute value of the errors. It is calculated as:
+# Mean Squared Error (MSE) is the mean of the squared errors and is calculated as:
+# Root Mean Squared Error (RMSE) is the square root of the mean of the squared errors:
+
+print("Mean absolute error: ", metrics.mean_absolute_error(y_test, y_pred))
+print("mean squared error: ", metrics.mean_squared_error(y_test, y_pred))
+print("Root mean square error: ", np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
--- part2_regression/simple_linear_regression2.py
+++ part2_regression/simple_linear_regression2.py
@@ -4,6 +4,9 @@ import pandas as pd
 import seaborn as sns
 # %matplotlib inline
 from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
 boston_dataset = load_boston()
 print(boston_dataset.keys())
@@ -60,4 +63,72 @@ features = ['LSTAT', 'RM']
 target = boston['MEDV']
 
 for i, col in enumerate(features):
-    pass
+    plt.subplot(1, len(features), i + 1)
+    x = boston[col]
+    y = target
+    plt.scatter(x, y, marker='o')
+    plt.title(col)
+    plt.xlabel(col)
+    plt.ylabel('MEDV')
+    plt.show()
+
+"""
+Observations:
+The prices increase as the value of RM increases linearly. There are few outliers and the data seems to be capped at 50.
+The prices tend to decrease with an increase in LSTAT. Though it doesn’t look to be following exactly a linear line.
+
+Preparing the data for training the model
+We concatenate the LSTAT and RM columns using np.c_ provided by the numpy library.
+"""
+
+X = pd.DataFrame(np.c_[boston['LSTAT'], boston['RM']], columns=['LSTAT','RM'])
+Y = boston['MEDV']
+
+"""
+Splitting the data into training and testing sets
+Next, we split the data into training and testing sets. We train the model with 80% of the samples and test with 
+the remaining 20%. We do this to assess the model’s performance on unseen data. To split the data we use 
+train_test_split function provided by scikit-learn library. 
+We finally print the sizes of our training and test set to verify if the splitting has occurred properly.
+"""
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=5)
+print(X_train.shape)
+print(X_test.shape)
+print(Y_train.shape)
+print(Y_test.shape)
+
+"""
+Training and testing the model
+We use scikit-learn’s LinearRegression to train our model on both the training and test sets.
+"""
+
+lin_model = LinearRegression()
+lin_model.fit(X_train, Y_train)
+
+
+"""
+Model evaluation
+We will evaluate our model using RMSE and R2-score.
+"""
+
+# model evaluation for training set
+y_train_predict = lin_model.predict(X_train)
+rmse = (np.sqrt(mean_squared_error(Y_train, y_train_predict)))
+r2 = r2_score(Y_train, y_train_predict)
+
+print("The model performance for training set")
+print("--------------------------------------")
+print('RMSE is {}'.format(rmse))
+print('R2 score is {}'.format(r2))
+print("\n")
+
+# model evaluation for testing set
+y_test_predict = lin_model.predict(X_test)
+rmse = (np.sqrt(mean_squared_error(Y_test, y_test_predict)))
+r2 = r2_score(Y_test, y_test_predict)
+
+print("The model performance for testing set")
+print("--------------------------------------")
+print('RMSE is {}'.format(rmse))
+print('R2 score is {}'.format(r2))
